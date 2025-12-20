34bc4aff7d6fcb1894734f5d7dcace4b584ff410
VivekPatel2117 <vivekpatel1374@gmail.com>
2025-12-20 17:47:26 +0530

    Added loggs for debugging

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
